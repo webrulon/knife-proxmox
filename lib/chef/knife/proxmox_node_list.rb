192f2e9f353e6ef5b03896e52c10dc95d3b7a910
Adam Enger <adamenger@gmail.com>
2013-10-08 22:58:56 -0500

    adding in Free Memory field in node list

--- lib/chef/knife/proxmox_node_list.rb
+++ lib/chef/knife/proxmox_node_list.rb
@@ -1,5 +1,6 @@
 require 'chef/knife/proxmox_base'
 
+
 class Chef
   class Knife
     class ProxmoxNodeList < Knife
@@ -15,23 +16,26 @@ class Chef
           ui.color('#', :bold),
           ui.color('Id', :bold),
           ui.color('Name', :bold),
-          ui.color('Size', :bold),
+          ui.color('Free Mem', :bold),
+          ui.color('Disk', :bold),
           ui.color('Uptime', :bold)
         ]
 
         @connection["cluster/resources?type=node"].get @auth_params do |response, request, result, &block|
+          puts response
           node_index = 1
-          JSON.parse(response.body)['data']
+
           JSON.parse(response.body)['data'].each { |entry|
             node_list << node_index.to_s
             node_list << entry['id']
             node_list << entry['node']
+            node_list << (entry['maxmem'].to_i/1024/1024*0.001 - entry['mem'].to_i/1024/1024*0.001).round(2).to_s + "GB"
             node_list << (entry['disk'].to_i/1048576).to_s + " GB"
             node_list << (entry['uptime'].to_i/86400).to_s + " Days"
             node_index+=1
           }
         end
-        puts ui.list(node_list, :uneven_columns_across, 5)
+        puts ui.list(node_list, :uneven_columns_across, 6)
       end
     end
   end
