8e19a8a36879a9654564dcf43ccf5ef214454b97
Adam Enger <adamenger@gmail.com>
2013-10-15 09:40:45 -0500

    uncommenting bootstrap option for OpenVZ. Tested and working with Jenkins cookbook

--- lib/chef/knife/proxmox_vm_create.rb
+++ lib/chef/knife/proxmox_vm_create.rb
@@ -44,7 +44,6 @@ class Chef
         :description => "(OpenVZ) - Root password to be set on the container"
 
       option :ip,
-        :short => "-I 192.168.7.1",
         :long  => "--ip 192.168.1.1",
         :description => "(OpenVZ) - IP Address to set on the container"
 
@@ -52,6 +51,31 @@ class Chef
         :short => "-t type",
         :long => "--vm_type type",
         :description => "Which type of vm to create (openvz or qemu)"
+
+      option :bootstrap,
+        :long => "--bootstrap",
+        :description => "(OpenVZ) - set this to automagically bootstrap after creating the node"
+
+      option :distro,
+        :short => "-d DISTRO",
+        :long => "--distro DISTRO",
+        :description => "Bootstrap a distro using a template; default is 'chef-full'",
+        :proc => Proc.new { |d| Chef::Config[:knife][:distro] = d },
+        :default => "chef-full"
+
+      option :run_list,
+        :short => "-r runlist",
+        :long => "--runlist",
+        :description => "(OpenVZ) - set this to automagically bootstrap after creating the node",
+        :proc => lambda { |o| o.split(/[\s,]+/) },
+        :default => []
+
+      option :environment,
+        :short=> "-e environment",
+        :long => "--environment environment",
+        :description => "Chef environment",
+        :proc => Proc.new {|env| Chef::Config[:knife][:environment] = env },
+        :default => '_default'
 
       def run
         connection
@@ -88,77 +112,77 @@ class Chef
         sleep(5)
         vm_start(vm_config[:vmid])
 
-        
-      #   # which IP address to bootstrap
-      #   bootstrap_ip_address = server_get_data(vm_id,'ip')
-      #   ui.msg("New Server #{vm_id} has IP Address: #{bootstrap_ip_address}")
-        
-      #   if bootstrap_ip_address.nil?
-      #     ui.error("No IP address available for bootstrapping.")
-      #     exit 1
-      #   end
+        def tcp_test_ssh(hostname)
+          tcp_socket = TCPSocket.new(hostname, 22)
+          readable = IO.select([tcp_socket], nil, nil, 5)
+          if readable
+            Chef::Log.debug("sshd accepting connections on #{hostname}, banner is #{tcp_socket.gets}")
+            yield
+            true
+          else
+            false
+          end
+          rescue Errno::ETIMEDOUT
+            false
+          rescue Errno::EPERM
+            false
+          rescue Errno::ECONNREFUSED
+            sleep 2
+            false
+          rescue Errno::EHOSTUNREACH
+            sleep 2
+            false
+          ensure
+            tcp_socket && tcp_socket.close
+        end
 
-      #   print(".") until tcp_test_ssh(bootstrap_ip_address) {
-      #     sleep @initial_sleep_delay ||= 10
-      #     puts("done")
-      #   }
+        def bootstrap_for_node(bootstrap_ip_address)
+          require 'chef/knife/bootstrap'
+          Chef::Knife::Bootstrap.load_deps
+          bootstrap = Chef::Knife::Bootstrap.new
+          bootstrap.name_args = config[:ip]
+          bootstrap.config[:run_list] = config[:run_list]
+          bootstrap.config[:environment] = config[:environment]
+          # bootstrap.config[:first_boot_attributes] = config[:first_boot_attributes]
+          bootstrap.config[:ssh_user] = "root"
+          bootstrap.config[:ssh_password] = config[:password]
+          # bootstrap.config[:identity_file] = config[:identity_file]
+          # bootstrap.config[:host_key_verify] = config[:host_key_verify]
+          bootstrap.config[:chef_node_name] = config[:vm_hostname]
+          # bootstrap.config[:prerelease] = config[:prerelease]
+          bootstrap.config[:bootstrap_version] = locate_config_value(:bootstrap_version)
+          bootstrap.config[:distro] = locate_config_value(:distro)
+          # bootstrap will run as root...sudo (by default) also messes up Ohai on CentOS boxes
+          # bootstrap.config[:use_sudo] = false
+          #bootstrap.config[:template_file] = locate_config_value(:template_file)
+          
+          puts bootstrap.config
+          bootstrap
+        end
 
-      #   # bootstrapping the node
-      #   if config[:bootstrap]
-      #     bootstrap_for_node(bootstrap_ip_address).run
-      #   else
-      #     ui.msg("Skipping bootstrap of the server because --no-bootstrap used as argument.")   
-      #   end 
-      
-      end
-      
-      # def tcp_test_ssh(hostname)
-      #   tcp_socket = TCPSocket.new(hostname, 22)
-      #   readable = IO.select([tcp_socket], nil, nil, 5)
-      #   if readable
-      #     Chef::Log.debug("sshd accepting connections on #{hostname}, banner is #{tcp_socket.gets}")
-      #     yield
-      #     true
-      #   else
-      #     false
-      #   end
-      # rescue Errno::ETIMEDOUT
-      #   false
-      # rescue Errno::EPERM
-      #   false
-      # rescue Errno::ECONNREFUSED
-      #   sleep 2
-      #   false
-      # rescue Errno::EHOSTUNREACH
-      #   sleep 2
-      #   false
-      # ensure
-      #   tcp_socket && tcp_socket.close
-      # end
+        if config[:bootstrap]
+          if config[:vm_type] == "openvz"
+            if vm_config[:ip_address].nil?
+              ui.error("No IP address available for bootstrapping.")
+              exit 1
+            else
+              ui.msg("New Server #{vm_config[:vmid]} has IP Address: #{vm_config[:ip_address]}")          
+            end
+            print(".") until tcp_test_ssh(vm_config[:ip_address]) {
+              sleep 10
+              puts("done")
+            }
 
-      # def bootstrap_for_node(bootstrap_ip_address)
-      #   require 'chef/knife/bootstrap'
-      #   Chef::Knife::Bootstrap.load_deps
-      #   bootstrap = Chef::Knife::Bootstrap.new
-      #   bootstrap.name_args = [bootstrap_ip_address]
-      #   bootstrap.config[:run_list] = config[:run_list]
-      #   bootstrap.config[:environment] = locate_config_value(:environment)
-      #   # bootstrap.config[:first_boot_attributes] = config[:first_boot_attributes]
-      #   bootstrap.config[:ssh_user] = "root"
-      #   bootstrap.config[:ssh_password] = config[:vm_password]
-      #   # bootstrap.config[:identity_file] = config[:identity_file]
-      #   # bootstrap.config[:host_key_verify] = config[:host_key_verify]
-      #   bootstrap.config[:chef_node_name] = config[:vm_hostname]
-      #   # bootstrap.config[:prerelease] = config[:prerelease]
-      #   bootstrap.config[:bootstrap_version] = locate_config_value(:bootstrap_version)
-      #   bootstrap.config[:distro] = locate_config_value(:distro)
-      #   # bootstrap will run as root...sudo (by default) also messes up Ohai on CentOS boxes
-      #   # bootstrap.config[:use_sudo] = false
-      #   bootstrap.config[:template_file] = locate_config_value(:template_file)
-        
-      #   pp bootstrap.config
-      #   bootstrap
-      # end
+            unless vm_config[:no_bootstrap]
+              bootstrap_for_node(vm_config[:ip_address]).run
+            else
+              ui.msg("Skipping bootstrap of the server because --no-bootstrap used as argument.")   
+            end
+          end
+        end
+      end  
     end
   end
 end
+     
+
